57c598f6c72c1ca01b552f7638fd4658d2a9f028
ZKGOHST-13 <badaouiislem440@gmail.com>
2026-09-02 21:42:36 +0200

    First Step

---
+++ back-end/src/server.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Islem ');
+});
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);
+})
